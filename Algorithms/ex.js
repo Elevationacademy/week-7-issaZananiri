dfd5bf440115a8e5ad17ba2aeceec6c0cb75c2f0
issaZananiri <https://github.com/issaZananiri>
2020-12-20 15:06:58 +0200

    Algorithms

---
+++ Algorithms/ex.js
@@ -0,0 +1,6 @@
+const printsStars = function(num) {
+  for ( let i = 1 ; i <= num ; i++ ) {
+    console.log( '*'.repeat(i) )
+  }
+}
+printsStars(5)
---
+++ Algorithms/ex2.js
@@ -0,0 +1,7 @@
+const printBackwardsStars = function( num ) {
+    let number = num;
+    while (number > 0) {
+        console.log( '*'.repeat(number) );
+        number--; }
+}
+printBackwardsStars(5)
